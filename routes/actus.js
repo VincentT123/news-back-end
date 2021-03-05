9e54a2041f0402924c672f104dae554e32caf237
VincentT123 <softeam2@FRP02954.softeam.com>
2021-03-05 17:06:53 +0100

    version V0.2

--- routes/actus.js
+++ routes/actus.js
@@ -35,6 +35,34 @@ router.get('/select/:id', (req, res) => {
         });
 });
 
+router.get('/carous', (req, res) => {
+
+     db.query('SELECT * FROM actus ORDER BY ID_actu ASC LIMIT 4',
+        function (err, rows) {
+            if (err) throw err;
+
+            res.json({
+                rows,
+                status: 200,
+                message: "liste d'actus carousel transmise avec succès"
+            })
+        });
+});
+
+router.get('/panel', (req, res) => {
+
+    db.query('SELECT * FROM actus ORDER BY ID_actu DESC LIMIT 4',
+       function (err, rows) {
+           if (err) throw err;
+
+           res.json({
+               rows,
+               status: 200,
+               message: "liste d'actus panel transmise avec succès"
+           })
+       });
+});
+
 router.get('/categ/:cat', (req, res) => {
 
     var cat = req.params.cat;
@@ -47,6 +75,34 @@ router.get('/categ/:cat', (req, res) => {
                 rows,
                 status: 200,
                 message: "liste d'une catégorie d'actus transmise avec succès"
+            })
+        });
+});
+
+router.get('/filactus', (req, res) => {
+
+    db.query('SELECT * FROM actus ORDER BY date_actu DESC LIMIT 5',
+        function (err, rows) {
+            if (err) throw err;
+
+            res.json({
+                rows,
+                status: 200,
+                message: "liste du fil d'actus transmise avec succès"
+            })
+        });
+});
+
+router.get('/pluslus', (req, res) => {
+
+    db.query('SELECT * FROM actus ORDER BY nblu DESC LIMIT 5',
+        function (err, rows) {
+            if (err) throw err;
+
+            res.json({
+                rows,
+                status: 200,
+                message: "liste des actus les plus lues transmise avec succès"
             })
         });
 });
