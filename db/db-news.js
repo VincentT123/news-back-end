cc476bfae7ed8bafdbd5e1ff921038b82eee4861
VincentT123 <softeam2@FRP02954.softeam.com>
2021-03-10 09:37:22 +0100

    version heroku

--- db/db-news.js
+++ db/db-news.js
@@ -1,10 +1,23 @@
 var mysql2 = require('mysql2');
 
+// var db = mysql2.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'projet_news'
+// });
 var db = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'projet_news'
+    host: 'lmc8ixkebgaq22lo.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user: 's4ru3fjdfelygh5l',
+    password: 'y55czqfkmxkqsecx',
+    database: 'pkwo4inobuh3u53h',
+    // dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
 });
 
 // connect to database
